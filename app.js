c2e0f87799f8d2994c1d888cced16219f6014a63
[dylan simerly] <[dylansimerly@yahoo.com]>
2022-07-11 13:09:04 -0400

    Display Formatted Date

--- app.js
+++ app.js
@@ -2,14 +2,26 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.render("list");
+
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-us", options);
+
+  res.render("list", {kindOfDay: day});
 });
+
 
 app.listen(3000, function() {
   console.log("server started on port 3000");
