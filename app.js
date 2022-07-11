118395bede70bce3401971cfc500f2d012c1c5c9
[dylan simerly] <[dylansimerly@yahoo.com]>
2022-07-11 12:41:10 -0400

    Server Running On Port 3000

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function() {
+  console.log("server started on port 3000");
+})
