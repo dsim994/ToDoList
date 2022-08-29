23a2f548ff8fff35a310affa6c8bec6e39632d0c
Dylan Simerly <dylansimerly@Dylans-MacBook-Pro-2.local>
2022-08-29 16:20:58 -0400

    Display Default Items From Database

--- app.js
+++ app.js
@@ -36,8 +36,20 @@ const itemsSchema = new mongoose.Schema ({
 const Item = new mongoose.model("Item", itemsSchema);
 
 
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
 
 
+const defaultItems = [item1, item2, item3];
 
 
 
@@ -45,11 +57,30 @@ const Item = new mongoose.model("Item", itemsSchema);
 //Home Route
 app.get("/", function(req, res) {
 
-  const day = date.getDate()
 
-  res.render("list", {listTitle: day, newListItems: items});
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length === 0) {
+
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully Updated The Document!");
+        }
+      });
+
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+
+  });
+
 
 });
+
+
+
 
 app.post("/", function(req, res) {
 
